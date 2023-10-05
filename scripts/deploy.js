db4075491649f16110afcfee698444ca0928e3c6
sumana10 <sumanaonlyme@gmail.com>
2023-10-05 21:32:33 +0530

    changed deployed script with transaction

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,36 +11,45 @@ const tokens = (n) => {
   return ethers.parseUnits(n.toString(), 'ether')
 }
 
+
 async function main() {
   // Setup accounts
-  const [deployer] = await hre.ethers.getSigners()
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying Dappazon...");
 
   // Deploy Dappazon
-  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
-  const dappazon = await Dappazon.deploy()
-  await dappazon.deployed()
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon");
+  const dappazon = await Dappazon.deploy();
 
-  console.log(`Deployed Dappazon Contract at: ${dappazon.address}\n`)
+  // Wait for the deployment event
+  await dappazon.waitForDeployment();
 
-  // Listing items...
-  for (let i = 0; i < items.length; i++) {
-    const transaction = await dappazon.connect(deployer.address).list(
-      items[i].id,
-      items[i].name,
-      items[i].category,
-      items[i].image,
-      tokens(items[i].price),
-      items[i].rating,
-      items[i].stock,
-    )
-    await transaction.wait()
-    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
-  }
+  console.log(`Dappazon contract deployed at: ${dappazon.target}`);
+
+ // Listing items...
+ for (let i = 0; i < items.length; i++) {
+  const transaction = await dappazon.connect(deployer).list(
+    items[i].id,
+    items[i].name,
+    items[i].category,
+    items[i].image,
+    tokens(items[i].price),
+    items[i].rating,
+    items[i].stock,
+  )
+  await transaction.wait()
+  console.log(`Listed item ${items[i].id}: ${items[i].name}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.log("Script execution complete.");
+}
+
+main()
+  .then(() => {
+    console.log("Script finished successfully.");
+  })
+  .catch((error) => {
+    console.error("Error executing script:", error);
+    process.exitCode = 1;
+  });
